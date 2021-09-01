f66deeb158d8039479f861517a549461112febf9
Bettega <tiagobettega.tb@gmail.com>
2021-09-01 09:03:57 -0300

    up
    arquivos

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const fs = require("fs");
+const config = require("./config.json");
+client.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`carregando ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+client.login(config.token);
